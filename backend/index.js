4a5d2ae6654d2eae55f3487c235a5bdaca2d0b45
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2023-01-28 17:43:24 +0100

    login i promjena sifre

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const mysql = require("mysql2")
+const cors = require("cors")
 
-const korisniciRoute = require('./routes/korisniciRoute')
+//const korisniciRoute = require('./routes/korisniciRoute')
 
 const app = express()
+
+app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -24,7 +28,139 @@ app.get("/", (req, res) => {
     res.json("Radiiiiii!!!!")
 })
 
-app.use('/login', korisniciRoute);
+
+//LOGIN
+app.post('/login', (req, res) => {
+    const korisnicko_ime = req.body.korisnicko_ime;
+    const sifra = req.body.sifra;
+
+    db.query(
+        "SELECT * FROM Korisnici JOIN Zaposlenici ON Korisnici.zaposlenik_id = Zaposlenici.id WHERE korisnicko_ime = ? AND sifra = ? AND (datum_otkaza IS NULL OR datum_otkaza > CURRENT_DATE)",
+        [korisnicko_ime, sifra],
+        (err, result) => {
+            if (err) {
+                res.send({ err: err });
+            }
+            if (result.length > 0) {
+                res.send(result)
+            } else {
+                res.send({ message: "Pogrešna kombinacija korisničko ime/šifra ili niste više zaposlenik i nemate pristup sistemu!" })
+            }
+        }
+    );
+});
+
+//PROMJENA ŠIFRE KORISNIKA
+app.post('/change-password', (req, res) => {
+    const currentPassword = req.body.currentPassword;
+    const newPassword = req.body.newPassword;
+    const username = req.body.username;
+
+    db.query("SELECT 1 FROM Korisnici WHERE korisnicko_ime = ?", [username], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: 'Error checking if user exists' });
+        }
+        if (result.length === 0) {
+            return res.status(400).json({ message: "Incorrect username." });
+        }
+        db.query("SELECT 1 FROM Korisnici WHERE korisnicko_ime = ? AND sifra = ?", [username, currentPassword], (err, result) => {
+            if (err) {
+                return res.status(500).json({ error: 'Error checking current password' });
+            }
+            if (result.length === 0) {
+                return res.status(400).json({ message: "Incorrect current password." });
+            }
+            db.query("UPDATE Korisnici SET sifra = ? WHERE korisnicko_ime = ?", [newPassword, username], (err, result) => {
+                if (err) {
+                    return res.status(500).json({ error: 'Error updating password' });
+                }
+                res.json({ message: "Successfully changed password." });
+            });
+        });
+    });
+});
+
+
+//PREGLED DOBAVLJAČA
+app.get('/dobavljaci', (req, res) => {
+    db.query("SELECT * FROM Dobavljaci", (err, result) => {
+        if (err) {
+            res.send({ err: err });
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+//IZMJENA DOBAVLJAČA
+app.put('/dobavljaci/:id', (req, res) => {
+    const id = req.params.id;
+    const naziv = req.body.naziv;
+    const jib = req.body.jib;
+    const pdv = req.body.pdv;
+    const broj_telefona = req.body.broj_telefona;
+    const kontakt_osoba = req.body.kontakt_osoba;
+    const email_adresa = req.body.email_adresa;
+    const datum_pocetka = req.body.datum_pocetka;
+    const datum_zavrsetka = req.body.datum_zavrsetka;
+
+    db.query(
+        "UPDATE Dobavljaci SET naziv = ?, jib = ?, pdv = ?, broj_telefona = ?, kontakt_osoba = ?, email_adresa = ?, datum_pocetka = ?, datum_zavrsetka = ? WHERE id = ?",
+        [naziv, jib, pdv, broj_telefona, kontakt_osoba, email_adresa, datum_pocetka, datum_zavrsetka, id],
+        (err, result) => {
+            if (err) {
+                res.send({ err: err });
+            } else {
+                res.send({ message: "Podaci o dobavljaču uspješno izmijenjeni!" });
+            }
+        }
+    );
+});
+
+//PREGLED SIROVINA
+app.get('/sirovine', (req, res) => {
+    db.query("SELECT * FROM Sirovine", (err, result) => {
+        if (err) {
+            res.send({ err: err });
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+
+//DODAVANJE SIROVINA
+app.post('/dodavanje-sirovina', (req, res) => {
+    const { naziv, kolicina, min_kolicina, cijena, jedinica_mjere, da_li_se_koristi, dobavljac_id } = req.body;
+
+    db.query("INSERT INTO Sirovine (naziv, kolicina, min_kolicina, cijena, jedinica_mjere, da_li_se_koristi, dobavljac_id) VALUES (?,?,?,?,?,?,?)", [naziv, kolicina, min_kolicina, cijena, jedinica_mjere, da_li_se_koristi, dobavljac_id], (err, result) => {
+        if (err) {
+            res.send({ err: err });
+        } else {
+            res.redirect('/sirovine');
+        }
+    });
+});
+
+
+//IZMJENA SIROVINA
+app.put('/sirovine/:id', (req, res) => {
+    const { id } = req.params;
+    const { naziv, kolicina, min_kolicina, cijena, jedinica_mjere, dobavljac_id } = req.body;
+    const query = `UPDATE Sirovine SET naziv = '${naziv}', kolicina = ${kolicina}, min_kolicina = ${min_kolicina}, 
+    cijena = ${cijena}, jedinica_mjere = '${jedinica_mjere}', dobavljac_id = ${dobavljac_id} WHERE id = ${id}`;
+    db.query(query, (err, result) => {
+        if (err) {
+            res.send({ err: err });
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+
+
+
 
 app.listen(8800, () => {
     console.log("Connected to backend!")
--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,80 @@
 import './App.css';
 import { useState } from 'react';
+import Axios from 'axios';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [korisnicko_ime, setUsername] = useState('');
+  const [sifra, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [loginStatus, setLoginStatus] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const [showChangePassword, setShowChangePassword] = useState(false);
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
 
-    fetch('/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ korisnicko_ime: username, pasvord: password }),
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.error) {
-        setError(data.error);
-      } else {
-        // handle successful login
-      }
-    })
-    .catch((error) => setError(error.message));
-  }
-  return (
-    <div className="App">
-      <div className="registracija">
-        <h1>Registracija na sistem</h1>
-        {error && <p>{error}</p>}
-          <label>Kosrisničko ime</label>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-          <label>Pasvord</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <button type="button" onClick={handleSubmit}>Registracija</button>
-      </div>
-    </div>
-  );
+const login = () => {
+  Axios.post("http://localhost:8800/login", {
+    korisnicko_ime: korisnicko_ime,
+    sifra: sifra,
+  }).then((response) => {
+    if(response.data.message){
+      setLoginStatus(response.data.message)
+    }else{
+      setLoginStatus(response.data[0].korisnicko_ime)
+    }
+    
+  });
 }
 
+  return (
+    <div className="App">
+  <div className="registracija">
+    <h1>Registracija na sistem</h1>
+    {error && <p>{error}</p>}
+      <label>Kosrisničko ime</label>
+      <input 
+          type="text" 
+          placeholder='Korisničko ime'
+          onChange={(e) => setUsername(e.target.value)} />
+      <label>Pasvord</label>
+      <input 
+          type="password" 
+          placeholder='Šifra' 
+          onChange={(e) => setPassword(e.target.value)} />
+      <button 
+          type="button" 
+          onClick={login}>Registracija</button>
+  </div>
+  <h1>{loginStatus}</h1>
+  <button className='dugmePasvord' type="button" onClick={() => setShowChangePassword(!showChangePassword)}>Promijeni šifru</button>
+  {showChangePassword && (
+    <div className="change-password">
+      <h2>Promjena šifre</h2>
+      <label>Trenutna šifra</label>
+      <input
+          type="password"
+          placeholder='Trenutna šifra'
+          onChange={(e) => setCurrentPassword(e.target.value)}
+      />
+      <label>Nova šifra</label>
+      <input
+          type="password"
+          placeholder='Nova šifra'
+          onChange={(e) => setNewPassword(e.target.value)}
+      />
+      <button
+          type="button"
+          onClick={changePassword}>Promijeni šifru</button>
+    </div>
+  )}
+  
+</div>
+
+  );
+  }
+
+  
 
 export default App;
